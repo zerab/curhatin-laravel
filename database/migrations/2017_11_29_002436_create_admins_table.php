04c497f5adc4b3e74535b2282e2a1794bb111d73
Abihu Haezer <abihu.haezer@gmail.com>
2017-11-30 07:20:14 +0700

    Initial Commit
    i#

---
+++ database/migrations/2017_11_29_002436_create_admins_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('firstname','32');
+          $table->string('lastname','32');
+          $table->string('email','64')->unique();
+          $table->date('birthdate')->nullable();
+          $table->string('birthplace','64');
+          $table->string('gender','1');
+          $table->string('phone','16')->unique();
+          $table->string('password');
+          $table->rememberToken();
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
